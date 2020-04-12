07fd7928000a520cbcf8f9d0833e7b889f0629f1
shelby palumbo <shelbypalumbo94@gmail.com>
2020-04-11 21:25:17 -0400

    Updates

--- precache-manifest.c28186a8859c3dc5276c7d7bd3a2f4c8.js
+++ precache-manifest.c28186a8859c3dc5276c7d7bd3a2f4c8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "732e3e3e0b1dfa0bf1c9841c35b1150a",
+    "revision": "a3395012e4cde999e6a274fb22ec4da3",
     "url": "/Employee-Directory/index.html"
   },
   {
-    "revision": "a4a1ec06da74e8463c32",
-    "url": "/Employee-Directory/static/css/main.f4518403.chunk.css"
+    "revision": "e1b918bf0fb529b99bf8",
+    "url": "/Employee-Directory/static/css/main.c69b321c.chunk.css"
   },
   {
     "revision": "9025e2fc259814f796b8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Employee-Directory/static/js/2.6506c695.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a4a1ec06da74e8463c32",
-    "url": "/Employee-Directory/static/js/main.2febebcd.chunk.js"
+    "revision": "e1b918bf0fb529b99bf8",
+    "url": "/Employee-Directory/static/js/main.b131e2f3.chunk.js"
   },
   {
     "revision": "a0464c722ef4ecaf280f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Employee-Directory/precache-manifest.4edee302f09ad158eab56b86ce967cb9.js"
+  "/Employee-Directory/precache-manifest.c28186a8859c3dc5276c7d7bd3a2f4c8.js"
 );
 
 self.addEventListener('message', (event) => {
